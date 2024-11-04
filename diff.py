89cb65e3d8757e112d8a49461693fe78e51e8209
Trolky <72431544+Trolky@users.noreply.github.com>
2024-11-04 09:36:50 +0100

    Update diff.py

--- diff.py
+++ diff.py
@@ -17,6 +17,49 @@ def max_difference_with_no_intermediates(sequence):
     
     return max_diff
 
+
+def max_difference_with_buckets(sequence):
+    n = len(sequence)
+    
+    if n < 2:
+        return 0
+    
+    global_min, global_max = min(sequence), max(sequence)
+    
+    if global_min == global_max:
+        return 0
+  
+    bucket_size = (global_max - global_min) / (n - 1)
+    buckets = [{'min': None, 'max': None} for _ in range(n)]
+    
+    for num in sequence:
+        index = int((num - global_min) / bucket_size)
+        
+        if buckets[index]['min'] is None or num < buckets[index]['min']:
+            buckets[index]['min'] = num
+        if buckets[index]['max'] is None or num > buckets[index]['max']:
+            buckets[index]['max'] = num
+    
+    max_diff = 0
+    previous_max = None
+    
+    for bucket in buckets:
+        if bucket['min'] is None:
+            continue
+        
+        if previous_max is not None:
+            max_diff = max(max_diff, abs(abs(bucket['min']) - abs(previous_max))) 
+        
+        previous_max = bucket['max']
+    
+    return max_diff
+
+
+sequence = [2, 5.3, 2.7, -20.1, 50]
+result = max_difference_with_buckets(sequence)
+print("Maximum difference:", result)
+
+
 sequence = [2, 5.3, 2.7, -20.1, 50]
 result = max_difference_with_no_intermediates(sequence)
-print("Maximum difference with no intermediates:", result)
+print("Maximum difference:", result)
